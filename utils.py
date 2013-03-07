8a67f5e50197d22edca201abd6d9efb30563ad81
Berker Peksag <berker.peksag@gmail.com>
2013-03-07 22:49:52 +0200

    Add SimpleCache.

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
 import requests
 
 from requests_oauthlib import OAuth1
+from werkzeug.contrib.cache import SimpleCache
 
 from settings import twitter
 
+cache = SimpleCache()
+
 
 def get_statuses():
+    statuses = cache.get("statuses")
+    if statuses is not None:
+        return statuses
     config = ("screen_name=jspyconf&count=5&exclude_replies=true&"
               "include_entities=false&include_rts=false&trim_user=true")
     url = "{}{}?{}".format(twitter.get("api_url"),
@@ -15,4 +21,5 @@ def get_statuses():
                   twitter.get("access_token"), twitter.get("access_token_secret"),
                   signature_type='auth_header')
     response = requests.get(url, auth=auth)
-    return response.json()
+    statuses = cache.set("statuses", response.json(), timeout=5 * 60)
+    return statuses
