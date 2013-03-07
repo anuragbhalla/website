5e74d215f53a2f8d17918fc07d45d4a6a6d3690d
Berker Peksag <berker.peksag@gmail.com>
2013-03-07 23:11:22 +0200

    Cleanup cache code.

--- utils.py
+++ utils.py
@@ -12,14 +12,21 @@ def get_statuses():
     statuses = cache.get("statuses")
     if statuses is not None:
         return statuses
-    config = ("screen_name=jspyconf&count=5&exclude_replies=true&"
-              "include_entities=false&include_rts=false&trim_user=true")
-    url = "{}{}?{}".format(twitter.get("api_url"),
-                           "statuses/user_timeline.json", config)
 
-    auth = OAuth1(twitter.get("consumer_key"), twitter.get("consumer_secret"),
-                  twitter.get("access_token"), twitter.get("access_token_secret"),
-                  signature_type='auth_header')
+    params = ("screen_name={screen_name}&count=5&exclude_replies=true&"
+              "include_entities=false&include_rts=false&trim_user=true")
+    url_fmt = "{url}{resource}?{params}".format
+    url = url_fmt(
+        url=twitter.get("api_url"),
+        resource="statuses/user_timeline.json",
+        params=params.format(screen_name=twitter.get("screen_name"))
+    )
+
+    auth = OAuth1(twitter.get("consumer_key"),
+                  twitter.get("consumer_secret"),
+                  twitter.get("access_token"),
+                  twitter.get("access_token_secret"),
+                  signature_type="auth_header")
     response = requests.get(url, auth=auth)
-    statuses = cache.set("statuses", response.json(), timeout=5 * 60)
-    return statuses
+    cache.set("statuses", response.json(), timeout=60 * 60)
+    return response.json()
