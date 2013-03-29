0fc1d5686788bfbff7e71d15958c0c00469ff191
Berker Peksag <berker.peksag@gmail.com>
2013-03-29 15:12:50 +0200

    Betiğin kaynağıyla ilgili not eklendi.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Bu muhteşem kodu özgür yazılım topluluğuyla paylaştıkları
+# için yeni Pardus ekibine teşekkürler.
+#
+# Kaynak: ftp://depo.pardus.org.tr/sources/build/files/pardus_config_kde.sh
 set -e
 
 echo "DEPLOY EDİLİYOR"
